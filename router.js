f02f670bc1b9bf8cc9cc13bb8d84aa59afc7c336
gaoyangy <13373935140@163.com>
2017-09-30 19:21:25 +0800

    fix router

--- router.js
+++ router.js
@@ -8,40 +8,54 @@
     {page:'/page1', tag:['todo','list']},
     {page:'/page2', tag:['list']}
   ]
+  let Arrtag = []
   if(util.history) {
     // 页面地址改变，更新页面
         fireUrlChange();
   }
   // hash值改变时更新页面
-    window.addEventListener('hashchange', url => {
+  window.addEventListener('hashchange', url => {
         fireUrlChange(url);
-    });
-    window.onpopstate=function(){
-      fireUrlChange();
-      location.reload() 
-    }
+  });
+
+  function add () {
+    var head= document.getElementsByTagName('html')[0]
+    tag.forEach(elem => {
+      var script= document.createElement('script');
+      script.type= 'riot/tag' 
+      script.src= `./tag/${elem}.tag`
+      head.appendChild(script)
+      setTimeout(function(){
+        if(!riot){
+          window.location.reload()
+        }else{
+          riot.compile(function() {
+            riot.mount('*')
+          })
+        }
+      },10)
+    })
+  }
   function fireUrlChange () {
   // 路由发生变化
   this.lastUrl = this.newUrl;
   PageTree.forEach(url => {
     let app = document.querySelector("#app")
     if(location.hash === '#'+ url.page || location.hash === url.page) {
+      let appdiv = document.querySelector("#app>div")
+      if (appdiv) {
+        app.innerHTML = ''
+      }
       console.log(url.page)
-      var head= document.getElementsByTagName('head')[0]
+      tag = url.tag
       url.tag.forEach(tag => {
         var template= document.createElement('div');
         template.innerHTML = `<${tag}></${tag}>`
         app.appendChild(template)
-        var script= document.createElement('script');
-        script.type= 'riot/tag' 
-        script.src= `./tag/${tag}.tag`
-        head.appendChild(script)
-      }, file)
-      var src=document.querySelector("script");
-      //head.removeChild(src);
-      //
+        //渲染html
+        add()
+    }, file)
     }
-    //
     function file (result,app) {
       if (!result) {
         // app.innerHTML = 'NO found'
